7406d9e701ce8fb6c411e2564d82d7c11fdde7ed
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-04-26 03:38:16 +0200

    Update cypunk1.py

--- Python/PyQt5/graphical user interface/Cyberpunk interface 1/cypunk1.py
+++ Python/PyQt5/graphical user interface/Cyberpunk interface 1/cypunk1.py
@@ -112,11 +112,9 @@
 #| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
         
 
-from PyQt5.QtWidgets import  QMainWindow, QWidget, QPushButton, QFrame, QLabel
-from PyQt5.QtGui import QIcon, QPainterPath,QRegion
-from PyQt5.QtCore import Qt, QPoint
-
-
+from PyQt5.QtWidgets import  QMainWindow, QWidget, QPushButton, QFrame, QLabel,QMessageBox,QVBoxLayout
+from PyQt5.QtGui import QIcon, QPainterPath,QRegion,QPainter, QColor,QPainterPath
+from PyQt5.QtCore import Qt, QPoint,QEvent
 
 
 #___  ____ _ _ _ ____ ____    ___  _    ____ _  _ ___
@@ -160,21 +158,28 @@ class cypunkTitle1(QWidget):
 
 
 
-# ____ ____ ____ ___  _  _ _ ____ ____ _       _  _ ____ ____ ____    _ _  _ ___ ____ ____ ____ ____ ____ ____ 
-# | __ |__/ |__| |__] |__| | |    |__| |       |  | [__  |___ |__/    | |\ |  |  |___ |__/ |___ |__| |    |___ 
-# |__] |  \ |  | |    |  | | |___ |  | |___    |__| ___] |___ |  \    | | \|  |  |___ |  \ |    |  | |___ |___ 
-                                                                                                             
+#_  _ ____ _ _  _    _ _ _ _ _  _ ___  ____ _ _ _ 
+#|\/| |__| | |\ |    | | | | |\ | |  \ |  | | | | 
+#|  | |  | | | \|    |_|_| | | \| |__/ |__| |_|_| 
+                                                 
+
 
 class cypunk1Window(QMainWindow):
     def __init__(self, title, window_size, btn_close, btn_minimize=None, btn_show=None, stylesheet_path=None):
         super().__init__()
-        self.init_UI(title, window_size, btn_close, btn_minimize, btn_show, stylesheet_path)
+        self.mwgui(title, window_size, btn_close, btn_minimize, btn_show, stylesheet_path)
 
-    def init_UI(self, title, window_size, btn_close, btn_minimize, btn_show, stylesheet_path):
+
+    # Graphical user interface of main window
+    def mwgui(self, title, window_size, btn_close, btn_minimize, btn_show, stylesheet_path):
         self.title = title  # initialize window title in a variable that can be used in another page
         self.btn_close = btn_close 
         self.btn_minimize = btn_minimize  
         self.btn_show = btn_show  
+
+        # Set Layout for the title (you can add self.layout.addWidget(YOUR_WIDGET) directly in other pages  )
+        self.init_Vlayout()
+
 
         #Load stylesheet if available
         if stylesheet_path:
@@ -227,11 +232,9 @@ class cypunk1Window(QMainWindow):
         self.close_button.clicked.connect(self.close_application) 
 
 
-
-
-
         # Create a new widget for the close button
         self.close_button_widget = QWidget()
+
         self.close_button_widget.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.Tool)
         self.close_button_widget.setAttribute(Qt.WA_NoSystemBackground, True)
         self.close_button_widget.setAttribute(Qt.WA_TranslucentBackground, True)
@@ -244,6 +247,16 @@ class cypunk1Window(QMainWindow):
         # Show the close button widget
         self.close_button_widget.show()
 
+
+
+    def init_Vlayout(self):
+        # Créez un QVBoxLayout pour le widget central
+        self.layout = QVBoxLayout(self.centralWidget())
+        self.layout.setContentsMargins(0, 30, 0, 0)
+
+    def Vlayout(self, widget):
+        # Add the given widget to the layout
+        self.layout.addWidget(widget)
 
 
     def moveEvent(self, event):
@@ -289,55 +302,60 @@ class cypunk1Window(QMainWindow):
     def close_application(self):
         self.close()  # close the application window, so it has to be used in main window.
 
+#_  _ ____ ____ ____ ____ ____ ____    ___  ____ _  _ 
+#|\/| |___ [__  [__  |__| | __ |___    |__] |  |  \/  
+#|  | |___ ___] ___] |  | |__] |___    |__] |__| _/\_ 
+                              
+                                                     
+class Cypunk1MessageBox(QMessageBox):
+    def __init__(self,stylesheet_path=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mbGui(stylesheet_path)  
+
+
+    def mbGui(self,stylesheet_path):
+        #Load stylesheet if available
+        if stylesheet_path:
+            with open(stylesheet_path, "r") as file:
+                stylesheet = file.read()
+                self.setStyleSheet(stylesheet)  
+     
+     
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)  # set window flags
+        self.setAttribute(Qt.WA_NoSystemBackground, True)  # set widget attribute for no system background
+        self.setAttribute(Qt.WA_TranslucentBackground, True)  # set widget attribute for translucent background
+   
+
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor("#505050"))
+
+        path = QPainterPath()
+        path.moveTo(30, 0)
+        path.lineTo(self.width() - 20, 0)
+        path.lineTo(self.width(), 20)
+        path.lineTo(self.width(), self.height() - 0)
+        path.lineTo(self.width() - 20, self.height())
+        path.lineTo(20, self.height())
+        path.lineTo(0, self.height() - 20)
+        path.lineTo(0, 0)
+        path.closeSubpath()
+
+        painter.drawPath(path)
+        
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragPosition = event.globalPos() - self.frameGeometry().topLeft()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            self.move(event.globalPos() - self.dragPosition)
+            event.accept()
 
 
 
-#_  _ ____ _ _ _    ___ ____    _  _ ____ ____ 
-#|__| |  | | | |     |  |  |    |  | [__  |___ 
-#|  | |__| |_|_|     |  |__|    |__| ___] |___ 
-                                              
-                                            
-
-#      HERE AN EXAMPLE HOW TO USE THIS CODE IN ANOTHER PAGE :
-#
-#         import sys
-#         from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout
-#         from cypunk1 import cypunk1Window
-#         
-#         class MyApp(cypunk1Window):
-#             def __init__(self):
-#         
-#                 #You can set here the things
-#                 super().__init__(
-#                     title="Test",
-#                     window_size="900x200",
-#                     btn_close="ico/close.png",
-#                     btn_minimize="ico/open.png",
-#                     btn_show="ico/hide.png",
-#                     stylesheet_path="style/style1.txt"
-#                 )
-#         
-#                 self.GUI()
-#         
-#             def GUI(self):
-#                 layout = QVBoxLayout()
-#         
-#                 button1 = QPushButton("Bouton 1")
-#                 layout.addWidget(button1)
-#         
-#                 button2 = QPushButton("Bouton 2")
-#                 layout.addWidget(button2)
-#         
-#                 button3 = QPushButton("Bouton 3")
-#                 layout.addWidget(button3)
-#         
-#                 self.central_widget.setLayout(layout)
-#         
-#         
-#         
-#         if __name__ == "__main__":
-#             app = QApplication(sys.argv)
-#             my_app = MyApp()
-#             my_app.show()
-#             sys.exit(app.exec_())
-#         
